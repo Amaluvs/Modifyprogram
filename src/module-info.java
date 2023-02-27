c4961736efc821639279636986a68431f2fc22e6
amalu <amalu@LAPTOP-3I9QMC8P>
2023-02-27 12:22:54 +0530

    hello java

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author amalu
+ *
+ */
+module Sample_program {
+}
---
+++ src/sample1/Additions.java
@@ -0,0 +1,16 @@
+package sample1;
+
+public class Additions {
+
+	public static void main(String[] args) 
+	{
+		int a=5;
+		int b=10;
+		int c=a+b;
+		System.out.println(c);
+		
+		
+
+	}
+
+}
